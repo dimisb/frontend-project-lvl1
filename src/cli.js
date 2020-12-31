323cad24df1625db3f27c06c07a7f91f50acfdf4
Dmitry Bunchuk <dmitrijs.buncuks@gmail.com>
2020-12-31 06:34:34 +0200

    added eslint

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,8 @@
 // greeting function
 
-import promptly from 'promptly';
-// Библиотека promptly работает в асинхронном режиме который мы еще не изучали
-// Для ее работы нужно выполнить две вещи:
-// 1. Добавить слово async перед определением функции
-// 2. Добавить слово await перед каждым вызовом метода prompt
-// В остальном код совершенно обычный
-export default async () => {
-  const name = await promptly.prompt('May I have your name?');
+import readlineSync from 'readline-sync';
+
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
