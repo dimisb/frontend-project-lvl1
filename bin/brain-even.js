d8d732fab0bc02e0b7ebc4000eba991bc6935036
Dmitry Bunchuk <dmitrijs.buncuks@gmail.com>
2021-01-02 23:36:44 +0200

    refactor even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,5 +2,4 @@
 
 import even from '../src/games/even.js';
 
-console.log('Welcome to the Brain Games!');
 even();
--- src/games/even.js
+++ src/games/even.js
@@ -2,31 +2,48 @@ import readlineSync from 'readline-sync';
 import { isEven, getRandomInt } from '../math.js';
 import { gameRounds, maxRandom } from '../index.js';
 
-const correctAnswer = (question) => {
-  if (isEven(question)) return 'yes';
+let question = 0;
+let answer = '';
+
+const giveGameObjective = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+};
+
+const correctAnswer = (q) => {
+  if (isEven(q)) return 'yes';
   return 'no';
 };
 
+const getAnswer = () => {
+  answer = readlineSync.question('Your answer: ');
+  return answer;
+};
+
+const askQuestion = () => {
+  question = getRandomInt(1, maxRandom);
+  console.log(`Question: ${question}`);
+  return question;
+};
+
+// game engine:
 export default () => {
   let i = 0;
   let succesCount = 0;
-  let question = 0;
-  let answer = '';
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  giveGameObjective();
   do {
-    question = getRandomInt(1, maxRandom);
-    console.log(`Question: ${question}`);
-    answer = readlineSync.question('Your answer: ');
-    if (answer === correctAnswer(question)) {
+    question = askQuestion();
+    answer = getAnswer();
+    if (answer === correctAnswer(question)) { // tweak: answer is str, not a num
       succesCount += 1;
       console.log('Correct!');
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer(question)}'. \nLet's try again, ${name}!`);
     }
     i += 1;
-  } while (answer === correctAnswer(question) && i < gameRounds);
+  } while (answer === correctAnswer(question) && i < gameRounds); // tweak: answer is str, not a num
   if (succesCount === 3) {
     console.log(`Congratulations, ${name}!`);
   }
